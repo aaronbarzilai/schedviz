fd7a3e7bf02f0dffb4c420bbbec91d7a87d36438
Aaron Barzilai <aaronbarzilai@gmail.com>
2016-03-09 12:17:05 -0500

    Correctly works, generates the file and uploads it to S3.

--- python/test/A00_Test_S3.py
+++ python/test/A00_Test_S3.py
@@ -6,6 +6,7 @@
 
 from __future__ import print_function
 import time, datetime
+import traceback
 
 import boto3 # For Amazon
 
@@ -25,6 +26,13 @@ if __name__ == "__main__":
 		# Print out bucket names
 		for bucket in s3.buckets.all():
 		    print(bucket.name)
+
+		# Let's make a made up file and put it on the bucket
+		filecontent = "<!DOCTYPE html>\n<html>\n<body>\n"
+		filecontent = filecontent + "Generated this content at " + str(datetime.datetime.now())+ " programmatically"
+		filecontent = filecontent + "</body></html>"
+
+		s3.Bucket('schedviz.com').put_object(Key='testfile.html', Body=filecontent,ContentType='text/html')
 
 		################################################ Main code ends here ################################################
 		
