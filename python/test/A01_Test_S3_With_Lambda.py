d5f0d8e28829387b34f5914142d8ea68352c75db
Aaron Barzilai <aaronbarzilai@gmail.com>
2016-03-09 13:40:14 -0500

    Tweaked to handle time in EST, time is always in UTC in servers.

--- python/test/A01_Test_S3_With_Lambda.py
+++ python/test/A01_Test_S3_With_Lambda.py
@@ -16,7 +16,8 @@ import json
 def main():
 	try:
 		print("Running main")
-		print("Now",datetime.datetime.now(),"\n")
+		print("Now",curr_est_time_as_string(),"EST (UTC-5)\n")
+		
 		start_time=time.time()
 
 		################################################ Main code starts here ################################################
@@ -30,19 +31,19 @@ def main():
 
 		# Let's make a made up file and put it on the bucket
 		filecontent = "<!DOCTYPE html>\n<html>\n<body>\n"
-		filecontent = filecontent + "Generated this content at " + str(datetime.datetime.now())+ " programmatically"
+		filecontent = filecontent + "Generated this content at " + curr_est_time_as_string() + " EST (UTC-5) programmatically with AWS lambda."
 		filecontent = filecontent + "</body></html>"
 
-		s3.Bucket('schedviz.com').put_object(Key='testfile.html', Body=filecontent,ContentType='text/html')
+		s3.Bucket('schedviz.com').put_object(Key='testfilelambda.html', Body=filecontent,ContentType='text/html')
 
 		################################################ Main code ends here ################################################
 		
 	except:
 		print(traceback.format_exc())
-		print("There was an error at",datetime.datetime.now())
+		print("There was an error at",curr_est_time_as_string(),"EST (UTC-5)\n")
 	finally:
 		end_time = time.time()
-		print("\nEnd at",datetime.datetime.now(),"\n")
+		print("\nEnd at",curr_est_time_as_string(),"EST (UTC-5)\n")
 		print("{0:.3f} secs elapsed, or {1:.3f} mins or {2:.3f} hours\n" \
 			.format(end_time-start_time,(end_time-start_time)/60.0,(end_time-start_time)/(60.0*60.0)))
 
@@ -53,8 +54,11 @@ def first_handler(event, context):
     #return event['key1']  # Echo back the first key value
     #raise Exception('Something went wrong')
 
-
-
+def curr_est_time_as_string():
+	dt = datetime.datetime.utcnow() - datetime.timedelta(hours=5)
+	dt_string = dt.strftime('%Y-%m-%d %H:%M:%S')
+	#print(dt_string)
+	return dt_string
 
 if __name__ == "__main__":
 	main()
